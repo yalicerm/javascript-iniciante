c3b3aa35c09dfb5313db5828008e832bbddf0181
Alice Resende <165073919+yalicerm@users.noreply.github.com>
2024-08-06 17:37:03 -0300

    remove child

---
+++ removeChild/index.js
@@ -0,0 +1,11 @@
+/*
+let lista = document.getElementById('lista')
+let val = document.querySelector('#val')
+let res = document.getElementById('res')
+lista.removeChild(val)
+*/
+
+//remover o elemento em si
+
+let titulo = document.querySelector('h1')
+titulo.remove()
